5d0635b0cf43511add685473deba92e6f889db40
Zita <buzasizita@gmail.com>
2020-04-07 14:26:02 +0100

    updated models

--- controllers/body_parts_controller.php
+++ controllers/body_parts_controller.php
@@ -1,8 +1,83 @@
 <?php
 
-class BodyPartsController {
+//class BodyPartsController {
+//
+//    public function create() {
+//        require_once('views/admin/bodyparts/createBodyPart.php');
+//    }
+//}
 
-    public function create() {
-        require_once('views/body-parts/create.php');
+
+class BodyPartsController
+{
+    public function readAll()
+    {
+        // we store all the posts in a variable
+        $products = BodyPart::all();
+        require_once('views/admin/bodyparts/readAllBodyPart.php');
     }
+
+    public function read()
+    {
+        // we expect a url of form ?controller=posts&action=show&id=x
+        // without an id we just redirect to the error page as we need the post id to find it in the database
+        if (!isset($_GET['id']))
+            return call('pages', 'error');
+
+        try {
+            // we use the given id to get the correct post
+            $product = BodyPart::find($_GET['id']);
+            require_once('views/admin/bodyparts/indexBodyPart.php');
+        } catch (Exception $ex) {
+            return call('pages', 'error');
+        }
+    }
+
+    public function create()
+    {
+        // we expect a url of form ?controller=products&action=create
+        // if it's a GET request display a blank form for creating a new product
+        // else it's a POST so add to the database and redirect to readAll action
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            require_once('views/admin/bodyparts/createBodyPart.php');
+        } else {
+            BodyPart::add();
+
+            $products = BodyPart::all(); //$products is used within the view
+            require_once('views/admin/bodyparts/readAllBodyPart.php');
+        }
+
+    }
+
+    public function update()
+    {
+
+        if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+            if (!isset($_GET['id']))
+                return call('pages', 'error');
+
+            // we use the given id to get the correct product
+            $product = BodyPart::find($_GET['id']);
+
+            require_once('views/admin/bodyparts/editBodyPart.php');
+        } else {
+            $id = $_GET['id'];
+            BodyPart::update($id);
+
+            $products = BodyPart::all();
+            require_once('views/admin/bodyparts/readAllBodyPart.php');
+        }
+
+    }
+
+    public function delete()
+    {
+        BodyPart::remove($_GET['id']);
+
+        $products = BodyPart::all();
+        require_once('views/admin/bodyparts/readAllBodyPart.php');
+    }
+
 }
+
+
--- views/admin/bodyparts/createBodyPart.php
+++ views/admin/bodyparts/createBodyPart.php
@@ -52,13 +52,13 @@
                 <a href="indexBodyPart.php" class="btn btn-info" id="adminBtn">Manage Body Part</a>
               <div class="container"> 
                     <div class="row h-25 d-inline-block"></div>
-                        <form>
+                        <form action="createBodyPart.php" method="POST" enctype="multipart/form-data">
                             <div class="form-group">
                                 <label for="BodyPartName">Body Part Name</label>
-                                <input type="text" class="form-control" id="BodyPartName" name="BodyPartName" placeholder="Legs">
+                                <input type="text" class="form-control" id="BodyPartName" name="part" placeholder="Legs">
                             </div>
                         </form>
-                        <button type="button" class="btn btn-info">Add Body Part</button>
+                        <a href="indexBodyPart.php" class="btn btn-info" id="adminBtn">Add Body Part</a>
                 </div>
             </div>  
         </div>     
