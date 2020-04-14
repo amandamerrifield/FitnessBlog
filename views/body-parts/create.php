a5c868c5519e3ca2a5c8d4be023f6811d09ae2da
Afrodyta <afrodytapudlo@hotmail.com>
2020-04-14 16:42:15 +0100

    Merge origin/master
    Conflicts:
    	views/admin/bodyparts/readAll.php
    	views/layout.php

--- views/body-parts/create.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <form>
-        <div>
-            <label for="body-part">body part</label>
-            <input name="part">
-        </div>
-        <button name="submit">Submit</button>
-    </form>
-</div>
