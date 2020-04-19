e55c5fa9c8b4d4854e66244c968fd7767ac3450f
Zita <buzasizita@gmail.com>
2020-04-19 17:00:20 +0100

    refactor index.php, logic remains the same

--- index.php
+++ index.php
@@ -1,29 +1,21 @@
 <?php
 require_once('connection.php');
 session_start();
-if (isset($_SESSION["username"])) {
-    if (isset($_GET['controller']) && isset($_GET['action'])) {
-        $controller = $_GET['controller'];
-        $action = $_GET['action'];
-    } else {
-        //these variables are set either by the query parameters or by default!
-        $controller = 'pages';
-        $action = 'home';
-    }
 
+if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controller = $_GET['controller'];
+    $action = $_GET['action'];
+} else {
+    //these variables are set either by the query parameters or by default!
+    $controller = 'pages';
+    $action = 'home';
+}
+
+if (isset($_SESSION["username"])) {
     if ((time() - $_SESSION['last_login_timestamp']) > 1000) {
         session_destroy();
     } else {
         $_SESSION['last_login_timestamp'] = time();
-    }
-} else {
-    if (isset($_GET['controller']) && isset($_GET['action'])) {
-        $controller = $_GET['controller'];
-        $action = $_GET['action'];
-    } else {
-        //these variables are set either by the query parameters or by default!
-        $controller = 'pages';
-        $action = 'home';
     }
 }
 
