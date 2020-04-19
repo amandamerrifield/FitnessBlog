648524967ee7b77967486f45e1a7a11e284f62d4
Afrodyta <afrodytapudlo@hotmail.com>
2020-04-19 18:55:27 +0100

    Merge origin/master
    Conflicts:
    	routes.php

--- views/admin/users/readOne.php
+++ views/admin/users/readOne.php
@@ -1,3 +1,4 @@
+<a href="index.php?controller=users&action=create" class="btn btn-info" id="adminBtn">Add User</a>
 <a href="index.php?controller=users&action=readOne" class="btn btn-info" id="adminBtn">Manage User</a>
 <div class="container">
     <div class="card">
@@ -5,26 +6,31 @@
         <div class="card-body">
             <table class="table fixed_header table-bordered table-responsive-sm table-striped text-center">
                 <thead>
-                    <tr>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Password</th>
-                        <th>Edit</th>
-                        <th>Delete</th>
-                    </tr>
+                <tr>
+                    <th>ID</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    
+                    <th>Edit</th>
+                    <th>Delete</th>
+                </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td><?php print $user->getUsername() ?></td>
-                        <td><?php print $user->getEmail() ?></td>
-                        <td><?php print $user->getPassword() ?></td>
-                        <td>
-                            <a href="index.php?controller=users&action=update&id=<?php print $user->getId() ?>">Edit</a>
-                        </td>
-                        <td>
-                            <a href="index.php?controller=users&action=delete&id=<?php print $user->getId() ?>">Delete</a>
-                        </td>
-                    </tr>
+                <tr>
+                    <td><?php print $user->getId() ?></td>
+                    <td><?php print $user->getUsername() ?></td>
+                    <td><?php print $user->getEmail() ?></td>
+                    <td><?php print $user->getPassword() ?></td>
+                    <td><?php print $user->getCreatedAt() ?></td>
+                    <td><?php print $user->getUpdatedAt() ?></td>
+                    <td>
+                        <a href="index.php?controller=users&action=update&id=<?php print $user->getId() ?>">Edit</a>
+                    </td>
+                    <td>
+                        <a href="index.php?controller=users&action=delete&id=<?php print $user->getId() ?>">Delete</a>
+                    </td>
+                </tr>
                 </tbody>
             </table>
         </div>
