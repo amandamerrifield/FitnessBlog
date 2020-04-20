0f82a5c55d42bd91606b936d0dd5e83710eef7b6
Zita <buzasizita@gmail.com>
2020-04-20 12:59:39 +0100

    revert change

--- index.php
+++ index.php
@@ -1,35 +1,7 @@
 <?php
 require_once('connection.php');
-require_once 'utilities.php';
 session_start();
-if (isset($_SESSION["username"]))
-{
-    if (isset($_GET['controller']) && isset($_GET['action'])) {
-    $controller = $_GET['controller'];
-    $action = $_GET['action'];
-    } else {
-    //these variables are set either by the query parameters or by default!
-    $controller = 'pages';
-    $action = 'home';
-    }
 
-    require_once('views/layout.php');
-    // slash works on both mac and win but \ only works on win
-
-    if((time() - $_SESSION['last_login_timestamp']) > 1000)
-    {
-        session_destroy();
-        echo 'you have been logged out';
-        
-    }else{
-        $_SESSION['last_login_timestamp'] = time();
-    }
-    
-    
-}else{
-    //require_once 'views/pages/home.php';
-//}
-    
 if (isset($_GET['controller']) && isset($_GET['action'])) {
     $controller = $_GET['controller'];
     $action = $_GET['action'];
@@ -39,31 +11,12 @@ if (isset($_GET['controller']) && isset($_GET['action'])) {
     $action = 'home';
 }
 
-require_once('views/layout.php');
-// slash works on both mac and win but \ only works on win
-
+if (isset($_SESSION["username"])) {
+    if ((time() - $_SESSION['last_login_timestamp']) > 1000) {
+        session_destroy();
+    } else {
+        $_SESSION['last_login_timestamp'] = time();
+    }
 }
 
-//
-//<?php
-//require_once('connection.php');
-//session_start();
-//
-//if (isset($_GET['controller']) && isset($_GET['action'])) {
-//    $controller = $_GET['controller'];
-//    $action = $_GET['action'];
-//} else {
-//    //these variables are set either by the query parameters or by default!
-//    $controller = 'pages';
-//    $action = 'home';
-//}
-//
-//if (isset($_SESSION["username"])) {
-//    if ((time() - $_SESSION['last_login_timestamp']) > 1000) {
-//        session_destroy();
-//    } else {
-//        $_SESSION['last_login_timestamp'] = time();
-//    }
-//}
-//
-//require_once('routes.php');
+require_once('routes.php');
