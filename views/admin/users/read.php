9174a7394728ca318cbf63387b0455d3f620c3b2
Zita <buzasizita@gmail.com>
2020-04-26 17:19:22 +0100

    remove photo update option for users

--- views/admin/users/read.php
+++ views/admin/users/read.php
@@ -11,7 +11,6 @@
 <h2 style="text-align:center">Our Team</h2>
 <br>
 <div class="container-fluid">
-
     <div class="row row-cols-1 row-cols-md-2">
         <?php foreach ($users as $user): ?>
             <?php if ($user->getAdmin() == 1) { ?>
