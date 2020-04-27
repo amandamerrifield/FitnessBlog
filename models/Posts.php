1effd2016dbdd035608c8eb6e3021d868846c2d3
Afrodyta <afrodytapudlo@hotmail.com>
2020-04-27 14:29:42 +0100

    fixed filters

--- models/Posts.php
+++ models/Posts.php
@@ -105,7 +105,7 @@ class Posts {
     public static function findByBodyPart($body_part_id) {
         $list = [];
         $db = Db::getInstance();
-        $req = $db->prepare('SELECT id, user_id, exercise_name, body_part_id, difficulty_id, description, created_at FROM posts WHERE body_part_id = :body_part_id');
+        $req = $db->prepare('SELECT id, user_id, exercise_name, body_part_id, difficulty_id, description, created_at, photo_type FROM posts WHERE body_part_id = :body_part_id');
         //the query was prepared, now replace :body_part_id with the actual $body_part_id value
         $req->execute(array('body_part_id' => $body_part_id));
         foreach ($req->fetchAll() as $posts) {
@@ -117,11 +117,11 @@ class Posts {
     public static function findByDifficulty($difficulty_id) {
         $list = [];
         $db = Db::getInstance();
-        $req = $db->prepare('SELECT id, user_id, exercise_name, body_part_id, difficulty_id, description, created_at FROM posts WHERE difficulty_id = :difficulty_id');
+        $req = $db->prepare('SELECT id, user_id, exercise_name, body_part_id, difficulty_id, description, created_at, photo_type FROM posts WHERE difficulty_id = :difficulty_id');
         //the query was prepared, now replace :id with the actual $id value
         $req->execute(array('difficulty_id' => $difficulty_id));
         foreach ($req->fetchAll() as $posts) {
-            $list[] = new Posts($posts['id'], $posts['user_id'], $posts['exercise_name'], $posts['body_part_id'], $posts['difficulty_id'], $posts['description'], $posts['created_at']);
+            $list[] = new Posts($posts['id'], $posts['user_id'], $posts['exercise_name'], $posts['body_part_id'], $posts['difficulty_id'], $posts['description'], $posts['created_at'],$posts['photo_type']);
         }
         return $list;
     }
