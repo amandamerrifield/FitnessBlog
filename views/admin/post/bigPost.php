99dacb2c6af5dd83c218926a56282d3cfd37d986
Zita <buzasizita@gmail.com>
2020-04-27 16:12:53 +0100

    added margin around sidebar

--- views/admin/post/bigPost.php
+++ views/admin/post/bigPost.php
@@ -38,7 +38,7 @@
 
             {foreach ($comments as $comment): ?>
                 <div class="media mb-4">
-                    <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+                    <div class="d-flex mr-3 rounded-circle"><i class="fas fa-user"></i></div>
                     <div class="media-body">
                         <h5 class="mt-0">Commenter Name</h5>
                         <?php echo htmlspecialchars_decode($comment->getContent(), ENT_QUOTES) ?>
